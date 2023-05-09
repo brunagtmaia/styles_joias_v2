bd462f80dc3f73ff346098af0a53c4270177cd9c
brunagtmaia <78162337+brunagtmaia@users.noreply.github.com>
2023-05-09 15:29:46 -0300

    Add files via upload

---
+++ script/script1.js
@@ -0,0 +1,25 @@
+function toggleArrow() {
+    var arrow = document.querySelector('.arrow');
+    arrow.classList.toggle('active');
+}
+// menu de tela pequena
+var menuToggle = document.getElementById('menuToggle');
+
+menuToggle.addEventListener('click', function() {
+  menuToggle.classList.toggle('active');
+});
+function menu_telap() {
+    var x = document.getElementById("menu_todo");
+    x.classList.toggle('active');
+}
+
+
+// Aguarde até que o DOM esteja pronto
+document.addEventListener("DOMContentLoaded", function() {
+  // Inicialize o carrossel
+  var carousel = new Flickity('.main-carousel', {
+      groupCells: true,
+      // ... outras opções do Flickity, se necessário ...
+  });
+});
+
